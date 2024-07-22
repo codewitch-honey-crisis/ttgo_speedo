57c8e648dff4ff7d30e0dc2847e4dd354b7cd3bf
codewitch-honey-crisis <gaztoast@gmail.com>
2024-07-22 04:53:45 -0700

    cleanup

--- src/display.cpp
+++ src/display.cpp
@@ -1,20 +1,9 @@
-#ifndef E_PAPER
-#ifdef ESP_PLATFORM
+
 #define LCD_IMPLEMENTATION
 #include <lcd_init.h>
-#endif
-#endif
+
 #include <display.hpp>
-#ifndef ESP_PLATFORM
-static lcd_t lcd;
-#endif
-#ifdef E_PAPER
-#ifdef ARDUINO
-arduino::lilygot54in7 epd;
-#else
-esp_idf::lilygot54in7 epd;
-#endif
-#endif
+
 
 // our transfer buffers
 // For screens with no DMA we only 
@@ -29,7 +18,7 @@ uint8_t* lcd_buffer1=nullptr;
 // the active screen
 screen_t* display_active_screen = nullptr;
 static bool display_sleeping = false;
-#if defined(ESP_PLATFORM) && !defined(E_PAPER)
+
 #ifdef LCD_DMA
 // only needed if DMA enabled
 static bool lcd_flush_ready(esp_lcd_panel_io_handle_t panel_io, 
@@ -39,10 +28,6 @@ static bool lcd_flush_ready(esp_lcd_panel_io_handle_t panel_io,
         display_active_screen->flush_complete();
     }
     return true;
-}
-#endif
-#if !defined(ESP_PLATFORM)
-static void uix_wait(void* state) {
 }
 #endif
 static void uix_flush(const gfx::rect16& bounds, 
@@ -58,11 +43,7 @@ static void uix_flush(const gfx::rect16& bounds,
 #endif
 }
 void display_init() {
-#ifdef LCD_PSRAM_BUFFER
-    lcd_buffer1 = (uint8_t*)heap_caps_malloc(lcd_buffer_size,MALLOC_CAP_SPIRAM);
-#else
     lcd_buffer1 = (uint8_t*)heap_caps_malloc(lcd_buffer_size,MALLOC_CAP_DMA);
-#endif
     if(lcd_buffer1==nullptr) {
         puts("Error allocating LCD buffer 1");
         while(1);
@@ -80,57 +61,10 @@ void display_init() {
     lcd_panel_init();
 #endif
 }
-#else
-void display_init() {
-#ifndef E_PAPER
-    lcd.initialize();
-#ifdef LCD_ROTATION
-    lcd.rotation(LCD_ROTATION);
-#endif
-#else
-    epd.initialize();
-#ifdef LCD_ROTATION
-    epd.rotation(LCD_ROTATION);
-#endif
-#endif
-}
-#ifdef LCD_DMA
-void uix_wait(void* state) {
-    gfx::draw::wait_all_async(lcd);
-}
-#endif
-void uix_flush(const gfx::rect16& bounds, 
-                    const void* bmp, 
-                    void* state) {
-    if(active_screen!=nullptr) {
-        gfx::const_bitmap<screen_t::pixel_type,screen_t::palette_type> cbmp(bounds.dimensions(),bmp,active_screen->palette());
-#ifndef E_PAPER
-        gfx::draw::bitmap_async(lcd,bounds,cbmp,cbmp.bounds());
-#else
-        gfx::draw::bitmap_async(epd,bounds,cbmp,cbmp.bounds());
-#endif
-#ifndef LCD_DMA
-        active_screen->flush_complete();
-#endif
-    }
-}
-#endif
 
 void display_update() {
     if(display_active_screen!=nullptr) {
-#ifdef E_PAPER
-        bool dirty = active_screen->dirty();
-        if(dirty) {
-            gfx::draw::suspend(epd);
-            epd.invalidate();
-        }
-        active_screen->update();
-        if(dirty) {
-            gfx::draw::resume(epd);
-        }
-#else
         display_active_screen->update();
-#endif
     }
 }
 
