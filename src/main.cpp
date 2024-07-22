ee91def5235751d0152d55f8ed301dc4ca868f97
codewitch-honey-crisis <gaztoast@gmail.com>
2024-07-21 21:17:15 -0700

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,7 @@ extern "C" void app_main();
 using namespace arduino;
 #else
 using namespace esp_idf;
+// shim for compatibility
 static uint32_t millis() {
     return pdTICKS_TO_MS(xTaskGetTickCount());
 }
@@ -29,14 +30,22 @@ static uint32_t millis() {
 
 using namespace gfx;
 using namespace uix;
+
+// two ttgo buttons
 using button_a_t = basic_button;
 using button_b_t = basic_button;
 button_a_t button_a_raw(0,10,true);
 button_b_t button_b_raw(35,10,true);
 multi_button button_a(button_a_raw);
 multi_button button_b(button_b_raw);
+
+// screen dimmer
 static lcd_miser<4> dimmer;
+
+// gps decoder
 static lwgps_t gps;
+
+// ui data
 static char speed_units[32];
 static char trip_units[16];
 static lwgps_speed_t gps_units;
@@ -49,6 +58,7 @@ static char loc_lon_buffer[64];
 static char loc_alt_buffer[64];
 static char stat_sat_buffer[64];
 static int current_screen = 0;
+// reads from Serial1/UART_NUM_1
 static size_t serial_read(char* buffer, size_t size) {
 #ifdef ARDUINO
     if(Serial1.available()) {
@@ -66,6 +76,7 @@ static size_t serial_read(char* buffer, size_t size) {
     return 0;
 #endif
 }
+// switch between imperial and metric units
 void toggle_units() {
     trip_counter = 0;
     if(gps_units==LWGPS_SPEED_KPH) {
@@ -78,6 +89,7 @@ void toggle_units() {
         strcpy(trip_units,"kilometers");
     }
 }
+// top button handler - switch screens
 void button_a_on_pressed_changed(bool pressed, void* state) {
     if(!pressed) {
         dimmer.wake();
@@ -86,24 +98,25 @@ void button_a_on_pressed_changed(bool pressed, void* state) {
         }
         switch(current_screen) {
             case 0:
-                puts("Speed screen");
+                // puts("Speed screen");
                 display_screen(speed_screen);
                 break;
             case 1:
-                puts("Trip screen");
+                // puts("Trip screen");
                 display_screen(trip_screen);
                 break;
             case 2:
-                puts("Location screen");
+                // puts("Location screen");
                 display_screen(loc_screen);
                 break;
             case 3:
-                puts("Stat screen");
+                // puts("Stat screen");
                 display_screen(stat_screen);
                 break;
         }
     }
 }
+// bottom button handler, toggle units
 void button_b_on_click(int clicks, void* state) {
     dimmer.wake();
     clicks&=1;
@@ -113,6 +126,7 @@ void button_b_on_click(int clicks, void* state) {
         trip_units_label.text(trip_units);
     }
 }
+// long handler - reset trip counter
 void button_b_on_long_click(void* state) {
     dimmer.wake();
     if(current_screen==1) {
@@ -121,42 +135,50 @@ void button_b_on_long_click(void* state) {
         trip_label.text(trip_buffer);
     }
 }
+// main application loop
 static void update_all() {
-    size_t read = serial_read(rx_buffer,sizeof(rx_buffer));
     static uint8_t sats_old = 0;
     static int speed_old = 0;
     if(gps.sats_in_use!=sats_old) {
-        printf("Satellites: %d/%d\n",(int)gps.sats_in_use,(int)gps.sats_in_view);
+        // printf("Satellites: %d/%d\n",(int)gps.sats_in_use,(int)gps.sats_in_view);
         sats_old = gps.sats_in_use;
     }
+    // each second, sample the speed and update the trip counter
     static uint32_t counter_ts = millis();
     if(millis()>=counter_ts+1000) {
         counter_ts = millis();
         trip_counter+=speed_old;
-        printf("Speed: %d %s\n",(int)speed_old,speed_units);
+        // printf("Speed: %d %s\n",(int)speed_old,speed_units);
         double trip = (double)trip_counter/(60.0*60.0);
-        printf("Trip: % .2f %s\n",trip,trip_units);
+        // printf("Trip: % .2f %s\n",trip,trip_units);
         snprintf(trip_buffer,sizeof(trip_buffer),"% .2f",trip);
         trip_label.text(trip_buffer);
         snprintf(stat_sat_buffer,sizeof(stat_sat_buffer),"%d/%d sats",(int)gps.sats_in_use,(int)gps.sats_in_view);
         stat_sat_label.text(stat_sat_buffer);
     }
+    // try to read from the GPS
+    size_t read = serial_read(rx_buffer,sizeof(rx_buffer));
     if(read>0) {
         lwgps_process(&gps,rx_buffer,read);
     }
+    // if we have GPS data:
     if(gps.is_valid) {
         float f = lwgps_to_speed(gps.speed,gps_units);
         if(f>MAX_SPEED) f=MAX_SPEED;
         int r = (int)roundf(f);
+        // if the speed isn't zero wake the screen up
         if((int)floorf(f)>0) {
             dimmer.wake();
         }
+        // fill speed_buffer
         itoa(r,speed_buffer,10);
         speed_label.text(speed_buffer);
+        // figure the needle angle
         int angle = (270 + ((int)roundf((f/MAX_SPEED)*180.0f)));
         while(angle>=360) angle-=360;
         speed_needle.angle(angle);
         speed_old = r;
+        // update the position data
         snprintf(loc_lat_buffer,sizeof(loc_lat_buffer),"lat: % .2f",gps.latitude);
         loc_lat_label.text(loc_lat_buffer);
         snprintf(loc_lon_buffer,sizeof(loc_lon_buffer),"lon: % .2f",gps.longitude);
@@ -164,10 +186,12 @@ static void update_all() {
         snprintf(loc_alt_buffer,sizeof(loc_lon_buffer),"alt: % .2f",gps.altitude);
         loc_alt_label.text(loc_alt_buffer);
     }
-    display_update();
+    // only screen zero auto-dims
     if(current_screen!=0) {
         dimmer.wake();
     }
+    // update the various objects
+    display_update();
     button_a.update();
     button_b.update();
     dimmer.update();
@@ -211,7 +235,7 @@ void loop() {
 static void loop_task(void* state) {
     while(true) {
         update_all();
-        vTaskDelay(5);
+        vTaskDelay(1);
     }
 }
 void app_main() {
